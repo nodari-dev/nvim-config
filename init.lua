3e0c11c16c89ef2683d556252751ed0e3a5d23fb
nodari-dev <nodari.pylypyshak@gmail.com>
2024-03-14 14:25:37 +0100

    updated cursor

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.wo.relativenumber = true
 vim.api.nvim_set_hl(0, 'CursorLine', { underline = true }) 
-
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
