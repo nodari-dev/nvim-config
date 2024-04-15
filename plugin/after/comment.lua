d9cd7cc1d469f55d8ebaddddbe53252975637c7c
nodari-dev <nodari.pylypyshak@gmail.com>
2024-04-15 22:55:54 +0200

    comment

--- plugin/after/comment.lua
+++ plugin/after/comment.lua
@@ -1,8 +1,2 @@
-require('Comment').setup({
-    toggler = {
-        ---Line-comment toggle keymap
-        line = '<C-L>',
-        ---Block-comment toggle keymap
-        block = '<C-B>',
-    },
-})
+require('Comment').setup()
+
