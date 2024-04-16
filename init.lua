c5339342d1dfcb0f3fa142d5bcfd276181ef1313
nodari-dev <nodari.pylypyshak@gmail.com>
2024-04-16 14:00:53 +0200

    minor updates

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@ vim.opt.nu = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.wo.relativenumber = true
-vim.opt.guicursor = "n-v-i-c:block-Cursor"
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
--- lua/carrotnvim/remap.lua
+++ lua/carrotnvim/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w", ":Prettier<CR>:w<CR>")
 vim.keymap.set("n", "<leader>gg", ":LazyGit")
 
