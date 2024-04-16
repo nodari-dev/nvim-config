1f08e60562dffbdd3f2aaaa60508b5fcda2b7df0
nodari-dev <nodari.pylypyshak@gmail.com>
2024-04-16 23:15:17 +0200

    deez nuts

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,6 +1,68 @@
+local home = os.getenv 'HOME'
+local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = workspace_path .. project_name
+
+local status, jdtls = pcall(require, 'jdtls')
+if not status then
+  return
+end
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+
 local config = {
-    cmd = { vim.fn.expand("~/Documents/java-lsp/bin/jdtls")},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+  cmd = {
+	'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+	'-javaagent:' .. home .. '/Documents/java-lombok/lombok.jar',
+	'-jar', vim.fn.glob(home .. '/Documents/java-lsp/plugins/org.eclipse.equinox.launcher_1.6.800.v20240304-1850.jar'),
+    '-configuration', home .. '/Documents/java-lsp/config_linux',
+    '-data', workspace_dir,
+},
+  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      extendedClientCapabilities = extendedClientCapabilities,
+      maven = {
+        downloadSources = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = 'all'
+	 	 },
+      },
+      format = {
+        enabled = false,
+      },
+    },
+  },
+
+  -- Language server `initializationOptions`
+  -- You need to extend the `bundles` with paths to jar files
+  -- if you want to use additional eclipse.jdt.ls plugins.
+  --
+  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+  --
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  init_options = {
+    bundles = {}
+  },
 }
+
 require('jdtls').start_or_attach(config)
 
--- plugin/after/dap.lua
+++ plugin/after/dap.lua
@@ -41,12 +41,13 @@ dap.configurations.kotlin = {
 
 }
 
-dap.configurations.java = {
-  {
-    type = 'java';
-    request = 'attach';
-    name = "Debug (Attach) - Remote";
-    hostName = "127.0.0.1";
-    port = 5005;
-  },
-}
+dap.adapters.java = function(callback)
+  -- FIXME:
+  -- Here a function needs to trigger the `vscode.java.startDebugSession` LSP command
+  -- The response to the command must be the `port` used below
+  callback({
+    type = 'server';
+    host = '127.0.0.1';
+    port = 8080;
+  })
+end
