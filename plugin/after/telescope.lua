f6c7cd4b3e272e522f456320be227bd83c230032
nodari-dev <nodari.pylypyshak@gmail.com>
2024-04-24 23:24:16 +0200

    telescoping little bit

--- plugin/after/telescope.lua
+++ plugin/after/telescope.lua
@@ -1,3 +1,14 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+
+require('telescope').setup {
+	defaults = {
+		layout_strategy = 'vertical',
+	},
+	pickers = {
+		find_files = {
+			theme = "dropdown",
+		}
+	},
+}
